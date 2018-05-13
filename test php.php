c2a0a0b323f24e6c65a31936d1d398df86d5b294
belajarcodegit <khairus.hendriansyah@gmail.com>
2018-05-13 12:38:21 +0700

    rev3

--- test php.php
+++ test php.php
@@ -6,6 +6,7 @@
         echo 'aku adalah uji coba git';  
         echo 'aku adalah uji coba git';  
         echo 'baru belajar Muklisio';  
-     
+        $datatanggal = DateTime::RFC822;
+        echo $datatanggal;
      ?>
 
