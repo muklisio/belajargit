6d41af60902a8310548d28beb1adcc3915bef82b
belajarcodegit <khairus.hendriansyah@gmail.com>
2018-05-13 12:35:45 +0700

    rev2

---
+++ cobaUploadgit.php
@@ -0,0 +1,9 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$param = DateTime::RFC822 ;
+echo $param;
