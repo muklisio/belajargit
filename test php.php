8fd8416c3be187c9242adba37630b1212b651cb1
belajarcodegit <khairus.hendriansyah@gmail.com>
2018-05-13 12:30:21 +0700

    rev1

---
+++ test php.php
@@ -0,0 +1,11 @@
+     <?php
+        // put your code here
+        echo 'aku adalah uji coba git';  
+        echo 'aku adalah uji coba git';  
+        echo 'aku adalah uji coba git';  
+        echo 'aku adalah uji coba git';  
+        echo 'aku adalah uji coba git';  
+        echo 'baru belajar Muklisio';  
+     
+     ?>
+
